10e6a51828bd4310878485f292812701b0d445d0
Nam Nguyen <namnguyn3019@gmail.com>
2021-04-01 18:14:41 -0500

    First commit

---
+++ MyFirstConsoleApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstConsoleApp.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "WebGentle";
+        }
+    }
+}
---
+++ MyFirstConsoleApp/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyFirstConsoleApp
+{
+    public class Startup
+    {
+       public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
